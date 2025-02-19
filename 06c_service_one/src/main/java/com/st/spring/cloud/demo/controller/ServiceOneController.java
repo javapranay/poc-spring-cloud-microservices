21dea7990c7e19600269e243eb9f1bf962f8e6ec
javapranay <java.pranay07@gmail.com>
2025-02-19 20:46:38 +0530

    06-eureka-config commit

---
+++ 06c_service_one/src/main/java/com/st/spring/cloud/demo/controller/ServiceOneController.java
@@ -0,0 +1,30 @@
+package com.st.spring.cloud.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class ServiceOneController {
+	
+	@Value("${service.name}")
+	private String serviceName;
+	
+	@GetMapping("/serviceone/prop")
+	public String getProp() {
+		return "Property name: " + serviceName;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
